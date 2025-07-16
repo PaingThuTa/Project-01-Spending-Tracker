ce6fcef2b2a662c96dcacd3170d9e128bdde2a86
PaingThuTa <paingthuta472@gmail.com>
2025-07-16 16:50:23 +0700

    initial project setup with package.json, index.html, and basic React structure

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Journal from './components/Journal';
+
+function App() {
+  return (
+    <div>
+      <h1>Spending Journal</h1>
+      <Journal />
+    </div>
+  );
+}
+
+export default App; 
