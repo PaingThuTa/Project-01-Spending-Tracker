28b974e87b665e8117635d91c25edef722973426
PaingThuTa <paingthuta472@gmail.com>
2025-07-21 20:53:52 +0700

    update homepage url + gh-pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Navigation from './components/Navigation';
 import AnalyticsDashboard from './components/AnalyticsDashboard';
 import Journal from './components/Journal';
